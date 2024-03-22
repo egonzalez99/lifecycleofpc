26318df4201de0118a3af95d95629233dbcc9267
egonzalez99 <112592570+egonzalez99@users.noreply.github.com>
2024-03-21 20:07:40 -0400

    making page v1
    thinking about adding morphing to the background

---
+++ lifecycle/js/morphebg.js
@@ -0,0 +1,5 @@
+background = {
+    'linear-gradient(to bottom right, #ff0000, #0000ff)',
+    'linear-gradient(to bottom right, #FF0000, #00FFFF)',
+    'linear-gradient(to bottom right, #414a4c, #FFB90F)'
+};
