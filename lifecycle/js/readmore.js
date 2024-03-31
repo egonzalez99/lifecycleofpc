e3828e6645d81657f438cbe1866fc1f90beedc25
egonzalez99 <112592570+egonzalez99@users.noreply.github.com>
2024-03-30 20:16:19 -0400

    impact page text and design done

--- lifecycle/js/readmore.js
+++ lifecycle/js/readmore.js
@@ -1,3 +1,4 @@
+/* ccreate the read more and read less button to interact with */
 function readMore() {
     let showText = document.getElementById("showText");
     let buttonText = document.getElementById("button");
@@ -9,5 +10,15 @@ function readMore() {
     else {
         showText.style.display = "none";
         buttonText.innerText = "Read More";
+    }
+}
+
+/* read the id of the text buttons */
+function readMore(id) {
+    var content = document.getElementById(id);
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
     }
 }
